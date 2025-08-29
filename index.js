1838994fc9c5b72a0141566298122cc0bc4c79f9
Roopesh O R <62459820+roopeshor@users.noreply.github.com>
2025-08-29 17:31:20 +0530

    added get/post req for checking
    Signed-off-by: Roopesh O R <62459820+roopeshor@users.noreply.github.com>

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const app = express()
 const PORT = 3000
 
 app.use(express.static("public"));
+app.use(express.json());
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
@@ -15,3 +17,20 @@ app.get("/checkin", (req, res) => {
 app.listen(PORT, () => {
 	console.log(`Checkin: http://localhost:${PORT}/checkin`);
 });
+
+
+app.post("/verify", (req, res) => {
+	try {
+		//TODO: to replace with actual database lookup
+		let id = req.body?.id;
+		let ids = ["SD_14234", "SD_3452", "SD_39503"];
+		let exists = false
+		if (ids.includes(id)) {
+			exists = true;
+		}
+
+		res.json({ exists })
+	} catch (error) {
+		res.status(500).json({ error: "Cant verify" });
+	}
+})
